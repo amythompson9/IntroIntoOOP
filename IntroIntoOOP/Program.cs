78a108352767807197ed551c5b3a5aceb145cd4a
amythompson9 <amythompson9@gmail.com>
2017-09-06 19:35:43 -0400

    Fields and Properties

--- IntroIntoOOP/Program.cs
+++ IntroIntoOOP/Program.cs
@@ -10,6 +10,14 @@ namespace IntroIntoOOP
     {
         static void Main(string[] args)
         {
+            Cat cat1 = new Cat();
+            Cat cat2 = new Cat("neko", "black", 3, true, 1);
+            Console.WriteLine(cat1.Name);
+            Console.WriteLine(cat2.Name);
+            cat1.Name = "gato";
+            Console.WriteLine(cat1.Name);
+            Console.WriteLine(cat2.Age);
+            Console.WriteLine(cat1.Age);
         }
     }
 }
