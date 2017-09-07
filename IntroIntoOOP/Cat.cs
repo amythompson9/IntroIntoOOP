69a2ab9d5feea87dcafa4be81bf269eb75d8744e
amythompson9 <amythompson9@gmail.com>
2017-09-06 20:25:39 -0400

    Added behaviors and properties

--- IntroIntoOOP/Cat.cs
+++ IntroIntoOOP/Cat.cs
@@ -13,7 +13,7 @@ namespace IntroIntoOOP
         private int furLength;
         private bool isMale;
         private int age;
-        private bool isHungry;
+        private bool isHungry; 
 
         public string Name
         {
@@ -34,18 +34,51 @@ namespace IntroIntoOOP
         {
             this.name = name;
         }
-        public Cat (int age)
+        public Cat(int age)
         {
             this.age = age;
         }
-        public Cat(string name, string furColor, int furLength, bool isMale, int age)
+        public string FurColor { get; set; }
+        public int FurLength { get; set; }
+
+        public Cat(string name, string furColor, int furLength, bool isMale, int age, bool isHungry)
         {
             this.name = name;
             this.furColor = furColor;
             this.furLength = furLength;
             this.age = age;
             this.isMale = isMale;
+            this.isHungry = isHungry;
+            
         }
-
+        public void Eat()
+        {
+            if(isHungry)
+            {
+                this.isHungry = false;
+                Console.WriteLine("I'm stuffed");
+            }
+            else if(!isHungry)
+            {
+                Console.WriteLine("I'm good. No food for me");
+            }
+        }
+        public void Run()
+        {
+            Console.WriteLine("I am running really fast!!");
+            this.isHungry = true;
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("I'm taking a nap.");
+        }
+        public void Scratching()
+        {
+            Console.WriteLine("Im sharpening my claws on your carpet because I destroy things because I'm a dick cat.");
+        }
+        public void Talk()
+        {
+            Console.WriteLine("My name is {0} and yes I can talk.",this.name);
+        }
     }
 }
--- IntroIntoOOP/Program.cs
+++ IntroIntoOOP/Program.cs
@@ -11,13 +11,24 @@ namespace IntroIntoOOP
         static void Main(string[] args)
         {
             Cat cat1 = new Cat();
-            Cat cat2 = new Cat("neko", "black", 3, true, 1);
+            Cat cat2 = new Cat("neko", "black", 3, true, 1, false);
             Console.WriteLine(cat1.Name);
             Console.WriteLine(cat2.Name);
             cat1.Name = "gato";
             Console.WriteLine(cat1.Name);
             Console.WriteLine(cat2.Age);
             Console.WriteLine(cat1.Age);
+
+            cat2.Eat();
+            cat2.Run();
+            cat2.Eat();
+            cat2.Sleep();
+            cat2.Scratching();
+            cat2.Talk();
+            cat1.Talk();
+            cat1.FurColor = "red";
+            Console.WriteLine(cat1.FurColor);
+            
         }
     }
 }
